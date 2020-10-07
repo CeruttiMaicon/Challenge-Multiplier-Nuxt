e8fdf9742b8f4acb97e80165ded79358e8ba828e
Maicon Cerutti <maicon.cerutti@facedigital.com.br>
2020-10-07 07:17:50 -0300

    criando usuário

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,12 +4,24 @@ export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     title: "Challenge-Multiplier-Nuxt",
-    meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "" }
+    meta: [{
+        charset: "utf-8"
+      },
+      {
+        name: "viewport",
+        content: "width=device-width, initial-scale=1"
+      },
+      {
+        hid: "description",
+        name: "description",
+        content: ""
+      }
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+    link: [{
+      rel: "icon",
+      type: "image/x-icon",
+      href: "/favicon.ico"
+    }]
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
@@ -30,7 +42,9 @@ export default {
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
     // https://go.nuxtjs.dev/bootstrap
-    "bootstrap-vue/nuxt",
+    ["bootstrap-vue/nuxt", {
+      icons: true,
+    }],
     // https://go.nuxtjs.dev/axios
     "@nuxtjs/axios",
     "@nuxtjs/auth-next"
