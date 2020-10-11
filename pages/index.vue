bf0954e4ee7d8ba8404caac3ec39363399357104
Maicon Cerutti <maicon.cerutti@facedigital.com.br>
2020-10-11 20:20:06 -0300

    Adicionado imagem home page desafio

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,7 @@
     <b-row class="text-center">
       <b-col>
         <h1 class="h1">Desafio Multiplier</h1>
+          <img src="~/assets/images/challenge-accepted.jpg" />
       </b-col>
     </b-row>
   </b-container>
